48413533d3d134f53719bf79e86504c04b5a121b
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2024-08-03 08:32:08 +0545

    website data

---
+++ app/data.js
@@ -0,0 +1,322 @@
+//Workshop data
+import mocktest from "./Assets/eventimg/mocktest.jpg";
+import frontendworkshop from "./Assets/eventimg/frontendworkshop.jpg";
+import futsal from "./Assets/eventimg/futsal.jpg";
+import graphicdesigning from "./Assets/eventimg/graphicdesigning.jpg";
+import itroadmap from "./Assets/eventimg/itroadmap.jpg";
+import workshopNET from "./Assets/eventimg/workshopNET.jpg";
+import reactEvent from "./Assets/eventimg/reactEvent.jpg";
+
+//Teams data
+import IMG1 from "./Team/keshab.jpg";
+import IMG2 from "./Team/sachin.jpg";
+
+import IMG4 from "./Team/suman.jpg";
+import IMG5 from "./Team/saradsir.jpg";
+import IMG6 from "./Team/sujanb.jpg";
+import IMG7 from "./Team/bibek.jpg";
+import IMG8 from "./Team/anuradha.jpg";
+import IMG9 from "./Team/MISSON.jpeg";
+import IMG10 from "./Team/sanchit.jpeg";
+import IMG11 from "./Team/bipin.jpg";
+import IMG12 from "./Team/kritan.jpg";
+import IMG13 from "./Team/pratik.jpg";
+import IMG14 from "./Team/shikshya.jpg";
+import IMG15 from "./Team/karuna.jpeg";
+import IMG16 from "./Team/govinda.jpeg";
+import IMG17 from "./Team/dikshya.jpeg";
+import IMG18 from "./Team/samir.jpg";
+import IMG20 from "./Team/urbish.png";
+import IMG21 from "./Team/SANGIT.png";
+
+import IMG19 from "./Team/jeevan.jpeg";
+
+import img1 from "./Assets/partners/1.png";
+import img2 from "./Assets/partners/2.png";
+import img3 from "./Assets/partners/3.png";
+import img4 from "./Assets/partners/4.jpg";
+import img5 from "./Assets/partners/5.jpg";
+import img6 from "./Assets/partners/6.png";
+import img7 from "./Assets/partners/7.png";
+import img8 from "./Assets/partners/8.png";
+import img9 from "./Assets/partners/9.jpg";
+import img10 from "./Assets/partners/tech.png";
+//Testimonials data
+import Bishan from "./Team/bishan.jpg";
+import Saroj from "./Team/saroj.jpg";
+import Sujan from "./Team/Sujan.jpg";
+import Manoj from "./Team/manoj.jpg";
+import Mahendra from "./Team/mahendra.jpg";
+import Prakash from "./Team/prakash.jpg";
+import Rabin from "./Team/Rabin.jpg";
+import sindhu from "./Team/sindhu.jpg";
+//Exporting Navbar Links
+export const Links = [
+  {
+    title: "Home",
+    path: "home",
+  },
+  {
+    title: "About",
+    path: "about",
+  },
+  {
+    title: "Events",
+    path: "events",
+  },
+  {
+    title: "Teams",
+    path: "teams",
+  },
+  {
+    title: "Reviews",
+    path: "myReview",
+  },
+  {
+    title: "Contact",
+    path: "FooterDiv",
+  },
+];
+
+//Exporting Event details
+export const products = [
+  {
+    id: 1,
+    title: "Mock Test 2079",
+    img: mocktest,
+  },
+  {
+    id: 2,
+    title: "Frontend Workshop",
+    img: frontendworkshop,
+  },
+  {
+    id: 3,
+    title: "Futsal Tournament",
+    img: futsal,
+  },
+  {
+    id: 4,
+    title: "Graphic Designing Workshop",
+    img: graphicdesigning,
+  },
+  {
+    id: 5,
+    title: "IT Roadmap Event",
+    img: itroadmap,
+  },
+  {
+    id: 6,
+    title: ".NET Workshop",
+    img: workshopNET,
+  },
+  {
+    id: 7,
+    title: "React Workshop",
+    img: reactEvent,
+  },
+];
+
+//Exporting Teams data
+export const teamdata = [
+  {
+    id: 1,
+    Name: "SUMAN BHATTARAI",
+    img: IMG4,
+    Post: "President",
+    Facebook: "https://www.facebook.com/suman.bhattarai.986",
+  },
+  {
+    id: 2,
+    Name: "SUJAN KHATRI",
+    img: IMG6,
+    Post: "Vice-President",
+    Facebook: "https://www.facebook.com/sujansince2003",
+  },
+  {
+    id: 8,
+    Name: "ANURADHA PANDEY",
+    img: IMG8,
+    Post: "Vice President",
+    Facebook: "https://www.facebook.com/profile.php?id=100035413025994",
+  },
+  {
+    id: 3,
+    Name: "SANCHIT PANDEY",
+    img: IMG10,
+    Post: "Secretary",
+    Facebook: "https://www.facebook.com/sanchitpandeyyy",
+  },
+  {
+    id: 4,
+    Name: "BIBEK ACHARYA",
+    img: IMG7,
+    Post: "Joint-Secretary",
+    Facebook: "https://www.facebook.com/Bivekacharya2059",
+  },
+  {
+    id: 5,
+    Name: "SHIKSHYA ACHARYA",
+    img: IMG14,
+    Post: "TREASURER",
+    Facebook: "https://www.facebook.com/Shikshyaacharyaa",
+  },
+  {
+    id: 13,
+    Name: "PRATIK ARYAL",
+    img: IMG13,
+    Post: "JOINT TREASURER ",
+    Facebook: "https://www.facebook.com/pratik.aryal.9615",
+  },
+  {
+    id: 6,
+    Name: "JEEVAN GHIMIRE",
+    img: IMG19,
+    Post: "Tech-Lead",
+    Facebook: "https://www.facebook.com/imjeevanghimire",
+  },
+  {
+    id: 7,
+    Name: "BIPIN KHATRI ",
+    img: IMG11,
+    Post: "Ass. Tech Lead",
+    Facebook: "https://www.facebook.com/im.bipo",
+  },
+  {
+    id: 12,
+    Name: "KRITAN ARYAL",
+    img: IMG12,
+    Post: "Event Manger",
+    Facebook: "https://www.facebook.com/aryal.kritan",
+  },
+  {
+    id: 15,
+    Name: "KARUNA PYAKUREL",
+    img: IMG15,
+    Post: "Event Manger",
+    Facebook: "https://www.facebook.com/karunakshetry.pyakurel",
+  },
+  {
+    id: 9,
+    Name: "GOVINDA SAGAR BHUSAL",
+    img: IMG16,
+    Post: "Executive Member",
+    Facebook: "https://www.facebook.com/aayush.bhusal.50",
+  },
+  {
+    id: 10,
+    Name: "DIKSHYA SHARMA",
+    img: IMG17,
+    Post: "Executive Member",
+    Facebook: "https://www.facebook.com/dikshya.sharma.9216",
+  },
+  {
+    id: 11,
+    Name: "MISSION PAUDEL",
+    img: IMG9,
+    Post: "Executive Member",
+    Facebook: "https://www.facebook.com/misson.paudel.3",
+  },
+
+  {
+    id: 14,
+    Name: "SAMIR ARYAL",
+    img: IMG18,
+    Post: "Executive Member",
+    Facebook: "https://www.facebook.com/samir.aryal.1428",
+  },
+  {
+    id: 16,
+    Name: "URBISH GHIMIRE",
+    img: IMG20,
+    Post: "Executive Member",
+    Facebook: "https://www.facebook.com/urbish.ghimire",
+  },
+  {
+    id: 17,
+    Name: "SANGIT GYAWALI",
+    img: IMG21,
+    Post: "Executive Member",
+    Facebook: "https://www.facebook.com/SangeeT.GyAwaLi52",
+  },
+];
+
+//Exporting testimonials data
+export const Partdata = [
+  { id: 10, Image: img10 },
+  { id: 3, Image: img3 },
+  { id: 4, Image: img4 },
+  { id: 1, Image: img1 },
+  { id: 2, Image: img2 },
+  { id: 5, Image: img5 },
+  { id: 6, Image: img6 },
+  { id: 7, Image: img7 },
+  { id: 8, Image: img8 },
+  { id: 9, Image: img9 },
+];
+
+export const Testidata = [
+  {
+    id: 1,
+    image: Rabin,
+    Name: "Rabin Pandey",
+    testimony: `Technology can be our closest companion, and technology can likewise be the greatest partypopper of our lives.It plays a major role in our lives.
+    As a part of CSIT Association of BMC, the most challenging part for me was to make the changes in directions, systems as well college guidelines which somehow got solved and which was possible by our team. Majority of innovation will be exhibit soon and lastly working with an energetic team leading by me was a wonderful experience ever in my lifetime.`,
+    Post: `Senior Advisor of CSITA-BMC`,
+  },
+  {
+    id: 1,
+    image: Bishan,
+    Name: "Bishan Bashyal",
+    testimony: `CSIT Association of BMC a group of highly energetic tech enthusiast, I remember founding this organization from scratch and now the new tenures took this organization to next level, more than i imagined. `,
+    Post: `IT Officer and Founder President of CSITA-BMC`,
+  },
+  {
+    id: 2,
+    image: Saroj, //We are importing objects so no JSX
+    Name: "Saroj Dahal",
+    testimony:
+      '"My first session on “Roadmap to IT Career” organized by CSIT Association of BMC was a memorable one . I felt a cosy aura while mentoring on the event. Truth to be told, I always had a good vibe with Butwal people and from this event,  I surely can say that , “ I felt it “  . Cheers to the Team"',
+    Post: `Fullstack Developer and CTO at Cliffbyte`,
+  },
+  {
+    id: 3,
+    image: Prakash,
+    Name: "Prakash Dhakal",
+    testimony:
+      "CSIT Association of BMC एउटा परिवार हो, म यो संस्थालाई, संस्था भन्दा पनि परिवार भन्न रुचाउने गर्छु। जहिले देखि यस संस्थाको जन्म भयो त्यसपछि नै सम्पुर्ण बिद्यार्थीको अभिभावकको रुपमा काम गरिरहेको छ,बिद्यार्थीहरुको आवाज उठाउने र सम्बोधन गराउने काम गरिरहेको छ। यस संस्थाले क्याम्पसमै उल्लेखनीय कामहरु त गरेको छ सङ्सङै बिद्यार्थी-बिद्यार्थी बिचको घनिष्ठ आत्मियता बढाउन महत्त्वपूर्ण योगदान गरेको छ। CSIT पढ्ने बिद्यार्थी एकजुट छन भन्ने सन्देश क्याम्पसभर दिएको छ। मैले आज सम्म धेरै संस्थामा काम गरेको छु,मलाइ ती संस्थामध्ये सबैभन्दा प्यारो संस्था CSITA-BMC नै हो।",
+    Post: `Former President at CSITA-BMC`,
+  },
+  {
+    id: 4,
+    image: Manoj,
+    Name: "Manoj Pokharel",
+    testimony:
+      "CSIT Association of BMC speaks a lot more about Whole student problem rather than just being a group of people, they always focus on unity, Co-operation, and strengths to develop the better relationship between student and administration",
+    Post: `President at CSIT Association of Nepal-Rupandehi`,
+  },
+  {
+    id: 5,
+    image: Mahendra,
+    Name: "Mahendra Kandel",
+    testimony:
+      "I consider CSIT Association of BMC as a group of leaders of tech community who are always eager to impart knowledge and has willingness to see the change, Despite of being an organization inside academic boundary it always had contributed to national tech community, Proud to be a part of it and kudos to the new team.",
+    Post: `Former President at CSITA-BMC`,
+  },
+  {
+    id: 6,
+    image: Sujan,
+    Name: "Sujan Khatiwada",
+    testimony:
+      "It was a nice moment to share the experience with curious IT people at CSIT Association, BMC.",
+    Post: `Fullstack Developer and founder at sathibhai.com`,
+  },
+  {
+    id: 7,
+    image: sindhu,
+    Name: "Sindhu Aryal",
+    testimony:
+      "Seeing the CSIT Association of BMC growing beautifully with the mission and vision of helping IT enthusiasts to find a way out of their IT career, widening the reach beyond the college and giving back to the community; gives me immense pleasure.Hats off to you guys for  your tireless efforts on shaping it in a way so that upcoming students don't have to face the same barriers as we did in the past. ",
+    Post: `Web Developer and President at Butwal Coder Girls`,
+  },
+];
