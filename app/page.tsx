3319cf001eb257ff2bb9428815ba518c1b874090
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2024-08-03 08:32:34 +0545

    layout and page component to render whole website

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,45 @@
+"use client";
 import Image from "next/image";
+import { useEffect, useState } from "react";
+import {
+  Testimonials,
+  Navigation,
+  Header,
+  About,
+  Teams,
+  Loader,
+  Footer,
+  List,
+  Tlist,
+} from "./components";
+import Aos from "aos";
+import "aos/dist/aos.css";
 
 export default function Home() {
-  return <div>hello</div>;
+  const [loader, setloader] = useState(true);
+  useEffect(() => {
+    Aos.init({ easing: "ease", duration: 700 });
+  });
+  useEffect(() => {
+    setTimeout(() => {
+      setloader(false);
+    }, 2000);
+  });
+  return (
+    <div>
+      {loader ? (
+        <Loader />
+      ) : (
+        <>
+          <Navigation />
+          <Header />
+          <About />
+          <List />
+          <Tlist />
+          {/* <Testimonials /> */}
+          <Footer />
+        </>
+      )}
+    </div>
+  );
 }
