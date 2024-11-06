148d4da9c29f5d1c48d8ea5dfdd72236052736eb
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2024-11-06 23:04:18 +0545

    API route for handling authentication

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,8 @@
-import NextAuth from "next-auth"
 
+import NextAuth from "next-auth";
+import { authOptions } from "@/lib/authOptions";
 
-const handler = NextAuth({})
+const handler = NextAuth(authOptions);
 
-export { handler as GET, handler as POST }
+export { handler as GET, handler as POST };
+
